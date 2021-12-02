04791973622a0f34ce9d99a8fd589394bcbc960a
Michael <michaelcaxias.dev@gmail.com>
2021-12-02 17:44:58 -0300

    Traduz nomes para o português

--- src/components/SelectColors.jsx
+++ src/components/SelectColors.jsx
@@ -7,17 +7,17 @@ import MenuItem from '@material-ui/core/MenuItem';
 import { MyContext } from '../context/Provider';
 
 const colors = [
-  { color: '#7986cb', name: 'Lavender', id: '1' },
-  { color: '#33b679', name: 'Sage', id: '2' },
-  { color: '#8e24aa', name: 'Grape', id: '3' },
+  { color: '#7986cb', name: 'Lavanda', id: '1' },
+  { color: '#33b679', name: 'Sálvia', id: '2' },
+  { color: '#8e24aa', name: 'Uva', id: '3' },
   { color: '#e67c73', name: 'Flamingo', id: '4' },
   { color: '#f6c026', name: 'Banana', id: '5' },
-  { color: '#f5511d', name: 'Tangerine', id: '6' },
-  { color: '#039be5', name: 'Peacock', id: '7' },
-  { color: '#616161', name: 'Graphite', id: '8' },
-  { color: '#3f51b5', name: 'Blueberry', id: '9' },
-  { color: '#0b8043', name: 'Basil', id: '10' },
-  { color: '#d60000', name: 'Tomato', id: '11' },
+  { color: '#f5511d', name: 'Tangerina', id: '6' },
+  { color: '#039be5', name: 'Pavão', id: '7' },
+  { color: '#616161', name: 'Grafite', id: '8' },
+  { color: '#3f51b5', name: 'Mírtilo', id: '9' },
+  { color: '#0b8043', name: 'Manjericão', id: '10' },
+  { color: '#d60000', name: 'Tomate', id: '11' },
 ];
 
 export default function BasicMenu() {
@@ -58,6 +58,7 @@ export default function BasicMenu() {
             key={ name }
             id={ id }
             className="color-picker-menu-item"
+            style={ { color } }
             onClick={ () => handleClose({ name, id, color }) }
           >
             <div className="color-picker" style={ { backgroundColor: color } } />
