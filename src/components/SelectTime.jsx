129e632e8ceb1452a183b7527763844d07e95a0b
Michael <michaelcaxias.dev@gmail.com>
2021-12-02 14:02:00 -0300

    Adiciona condição caso o número seja 0

--- src/components/SelectTime.jsx
+++ src/components/SelectTime.jsx
@@ -3,13 +3,22 @@ import React, { useState } from 'react';
 import Select from 'react-select';
 
 const ONE_HOUR = 60;
-const MAX_TIME = [...Array(ONE_HOUR + 1).keys()].slice(1);
+const MAX_TIME = [...Array(ONE_HOUR + 1).keys()];
 const TEN = 10;
 
-const OPTIONS = MAX_TIME.map((time) => ({
-  value: time,
-  label: `${time < TEN ? `${time} minuto` : `${time} minutos`}`,
-}));
+console.log(MAX_TIME);
+
+const OPTIONS = MAX_TIME.map((time) => {
+  const moreThanZero = {
+    value: time,
+    label: `${time < TEN ? `${time} minuto` : `${time} minutos`}`,
+  };
+  const zero = {
+    value: time,
+    label: 'Na mesma hora',
+  };
+  return time === 0 ? zero : moreThanZero;
+});
 
 export default function SelectTime() {
   const [minutes, setMinutes] = useState(OPTIONS[0].value);
