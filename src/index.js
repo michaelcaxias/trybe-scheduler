2a1d42e0fa3cfc3ca4a3cb60c893a06cf0a7302e
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-12 18:41:53 -0300

    Adiciona cores em variáveis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './styles/index.scss';
 import App from './App';
 import { Provider } from './context/Provider';
 
