75ba88021f70d1d23382e2e0dd4489196ea93052
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-16 17:39:05 -0300

    Adiciona olhos a marte

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,9 +5,10 @@ import rocket from '../images/rocket.svg';
 export default function Loading() {
   return (
     <section className="loading-section">
+      <h1>Carregando...</h1>
       <div className="mars">
-        <div className="eyes" />
-        <div className="mounth" />
+        <div className="first-eye" />
+        <div className="second-eye" />
       </div>
       <div className="orbit">
         <img
