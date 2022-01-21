2fb20755deaae4ce895e4230393769f06c16d813
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-21 11:31:36 -0300

    Textarea: ajusta tamanho

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import '../styles/TextField.scss';
 
 export default function TextField() {
-  return <div contentEditable />;
+  return <div contentEditable className="divtexto" />;
 }
