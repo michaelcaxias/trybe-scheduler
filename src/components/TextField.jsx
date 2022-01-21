269c619c6c4e6de2ada01a8678d92a279e873bb2
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-21 12:15:30 -0300

    Remove focus e adiciona placeholder

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,10 +1,15 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import '../styles/TextField.scss';
 
-export default function TextField() {
+export default function TextField({ placeholder }) {
   return (
     <div className="text-field">
-      <div contentEditable />
+      <div contentEditable data-text={ placeholder } />
     </div>
   );
 }
+
+TextField.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+};
