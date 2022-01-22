6e82ae74cd9421dde42de429eeced9c0052d186d
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-22 15:12:50 -0300

    Adiciona função para capturar comprimento do textarea

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,16 +1,29 @@
+/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/TextField.scss';
 
 export default function TextField({ placeholder, refElement, label, disabled }) {
+  const [textAreaLength, changeTextAreaLength] = useState(0);
+
+  const getElementLength = () => {
+    const refLength = refElement ? refElement.current.innerText.length : 0;
+    changeTextAreaLength(refLength);
+  };
   return (
     <div className="text-field">
-      <div className="label-text">Agenda do Dia</div>
+      <div
+        className={ textAreaLength ? 'label-text focus' : 'label-text' }
+      >
+        Agenda do Dia
+
+      </div>
       <div
         ref={ refElement }
         contentEditable
         data-text={ placeholder }
         disabled={ disabled }
+        onKeyUp={ getElementLength }
         role="textbox"
         tabIndex="0"
         aria-label={ label }
