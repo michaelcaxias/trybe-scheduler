5652488f3f3f9e324f9b1179e947273aa2658f26
Angélica Silva <angelica.silva9323@gmail.com>
2022-01-27 18:20:15 -0300

    Implementada descrição com mais informações
    -> Implementado o link do slido;
    -> Implementado o link dos forms dários;
    -> Implementado captura das informações extras de cada evento do dia;
    -> Implementado estilização.

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,41 @@
 /* eslint-disable no-restricted-globals */
 const filterTime = (string, regex) => string.match(regex);
 
+const createDescription = (line, links) => {
+  const linksSlido = links.filter((link) => link.includes('sli.do'));
+  const linksForms = links.filter((link) => link.includes('typeform'));
+  const optionalLine = /[[,(]\*[),\]]/.test(line) // regex para capturar (*) ou [*]
+    ? '<b>Momento opcional.</b>\n'
+    : '';
+  const slido = /slido/i.test(line)
+    ? `<b>Slido:</b> ${linksSlido.shift()}\n`
+    : '';
+  const forms = /forms/i.test(line)
+    || /feedback/i.test(line)
+    || /formulário/i.test(line)
+    || /form/i.test(line)
+    ? `<b>Forms:</b> ${linksForms.shift()}\n`
+    : '';
+  let moreInfo = line.includes('Desc.:')
+    ? line.split('Desc.:')[1]
+    : '';
+  if (moreInfo !== '' && !moreInfo.includes('///')) {
+    moreInfo = `<b>Informação:</b> ${moreInfo.replace(/\s\s+/g, ' ').trim()}`;
+  }
+  if (moreInfo.includes('///')) {
+    moreInfo = `<b>Informações:</b>\n${moreInfo.split('///')
+      .map((elem) => elem.replace(/\s\s+/g, ' ')
+        .trim()).join('\n')}`;
+  }
+
+  return {
+    optionalLine,
+    slido,
+    forms,
+    moreInfo,
+  };
+};
+
 export const filterString = (string, links) => {
   const regex = /(([0-1]\d|2[0-4])(h|:)([0-5]\d)?)/ig;
   const linksZoom = links.filter((link) => link.includes('zoom'));
@@ -10,16 +45,27 @@ export const filterString = (string, links) => {
     .map((line) => {
       const [startTime, endTime] = filterTime(line, regex)
         .map((hour) => hour.replace(/h(\d\d)?/i, (_, min) => (min ? `:${min}` : ':00')));
-      const title = line
-        .replace(/-/g, '').trim();
-      const optionalLine = /[[,(]\*[),\]]/.test(line); // regex para capturar (*) ou [*]
-      const location = /zoom/i.test(line) ? linksZoom.shift() : 'Remote';
+      let title = line.includes('Desc.:')
+        ? line.split('Desc.:')[0]
+        : line.replace(/-/g, '').trim();
+      title = title.includes('///')
+        ? title.split('///')[0]
+        : title.replace(/\s\s+/g, ' ').trim();
+      const location = /zoom/i.test(line)
+        ? linksZoom.shift()
+        : 'Remote';
+      const {
+        optionalLine,
+        slido,
+        forms,
+        moreInfo } = createDescription(line, links);
+
       return {
         title,
         startTime,
         location,
         endTime: endTime || startTime,
-        description: optionalLine ? 'Momento opcional' : '',
+        description: `${optionalLine}${slido}${forms}${moreInfo}`,
       };
     });
 };
