010257d52553e784be5a48e098097a9b2dd93920
Michael Caxias <79621661+michaelcaxias@users.noreply.github.com>
2022-02-20 21:48:07 -0300

    fix: edit sensitive scopes (#51)

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -9,7 +9,7 @@ const { REACT_APP_CLIENT_ID } = process.env;
 const { REACT_APP_API_KEY } = process.env;
 
 const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
-const SCOPES = 'https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.calendars.readonly https://www.googleapis.com/auth/userinfo.profile';
+const SCOPES = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.profile';
 
 export const MyContext = createContext();
 
