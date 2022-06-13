b1ba7176866cb3f87bf76ac64b8ca9622813c66f
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-12 22:28:55 -0300

    :sparkles: feat: utiliza menu de mui

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
-export default function Menu() {
+export default function NavMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const generateSquares = () => {
     const squares = [];
     const min = 1;
@@ -12,5 +23,29 @@ export default function Menu() {
     return squares;
   };
 
-  return <Button className="mui-menu">{generateSquares()}</Button>;
+  return (
+    <Button
+      id="fade-button"
+      className="mui-menu"
+      aria-controls={ open ? 'basic-menu' : undefined }
+      aria-haspopup="true"
+      aria-expanded={ open ? 'true' : undefined }
+      onClick={ handleClick }
+    >
+      {generateSquares()}
+      <Menu
+        id="basic-menu"
+        anchorEl={ anchorEl }
+        open={ open }
+        onClose={ handleClose }
+        MenuListProps={ {
+          'aria-labelledby': 'basic-button',
+        } }
+      >
+        <MenuItem onClick={ handleClose }>Profile</MenuItem>
+        <MenuItem onClick={ handleClose }>My account</MenuItem>
+        <MenuItem onClick={ handleClose }>Logout</MenuItem>
+      </Menu>
+    </Button>
+  );
 }
