baac5ea2f71306a38aec8a34ca52a93d79308431
João Pster <joaopsterdev@gmail.com>
2022-10-26 19:27:25 -0300

    dev: atualizado para react 18, atualizado eslint, instalado o framer-motion

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef } from 'react';
 import MuiAlert from '@mui/material/Alert';
 
+// eslint-disable-next-line react/display-name
 const Alert = forwardRef((props, ref) => (
   <MuiAlert elevation={ 6 } ref={ ref } variant="filled" { ...props } />
 ));
