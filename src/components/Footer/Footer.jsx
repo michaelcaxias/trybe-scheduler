5c98fa3d183112ca92fd3f6f620adff7d21a8421
João Pster <joaopsterdev@gmail.com>
2022-10-27 00:03:26 -0300

    feat: adicionado politicas de privacidade e termos de uso no footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AiFillGithub } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 import './Footer.scss'
 
@@ -9,6 +10,11 @@ const Footer = () => {
       <p>Desenvolvido em React, com ❤️ por <a href="https://github.com/michaelcaxias/trybe-schedule/graphs/contributors">Todos os Colaboradores</a>.</p>
 
       <div className="app__footer-links">
+        <nav className="privacy-and-terms">
+          <Link to="/privacy-policy">Políticas de Privacidade</Link>
+          <Link to="/terms-and-conditions">Termos de Uso</Link>
+        </nav>
+
         <a
           href="https://github.com/michaelcaxias/trybe-schedule"
           target="_blank"
