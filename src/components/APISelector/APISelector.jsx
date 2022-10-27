dff6ae5ab4682f45387f4abcbeb8919b515ac01a
João Pster <joaopsterdev@gmail.com>
2022-10-27 14:05:24 -0300

    dev: removido antigo generate squares

--- src/components/APISelector/APISelector.jsx
+++ src/components/APISelector/APISelector.jsx
@@ -19,16 +19,6 @@ export default function APISelector() {
     setAnchorEl(null);
   };
 
-  const generateSquares = () => {
-    const squares = [];
-    const min = 1;
-    const max = 9;
-    for (let index = min; index <= max; index += min) {
-      squares.push(<div key={ index } className="square" />);
-    }
-    return squares;
-  };
-
   return (
     <Button
       className="mui-menu"
