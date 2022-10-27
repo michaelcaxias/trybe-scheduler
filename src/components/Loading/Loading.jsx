765133259198c004b93e600b471357c169f2f3df
João Pster <joaopsterdev@gmail.com>
2022-10-26 23:44:21 -0300

    feat: implementada nova tela de loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -6,18 +6,21 @@ import './Loading.scss';
 export default function Loading() {
   return (
     <section className="loading-section">
-      <h1>Carregando...</h1>
-      <div className="mars">
-        <div className="first-eye" />
-        <div className="second-eye" />
-      </div>
-      <div className="orbit">
-        <img
-          className="rocket"
-          src={ images.rocket }
-          alt="rocket"
-        />
-      </div>
-    </section>
+    <div id="stars"></div>
+    <div id="stars2"></div>
+    <div id="stars3"></div>
+
+    <div className="mars">
+      <div className="first-eye" />
+      <div className="second-eye" />
+    </div>
+    <div className="orbit">
+      <img
+        className="rocket"
+        src={ images.rocket }
+        alt="rocket"
+      />
+    </div>
+  </section>
   );
 }
